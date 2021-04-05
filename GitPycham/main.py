d8697552cbc47c79c4ca77b3467f51940c081ab4
YeEun Lee <37532204+RulLu16@users.noreply.github.com>
2021-04-05 17:23:23 +0900

    Updates 2 on Github Only

--- GitPycham/main.py
+++ GitPycham/main.py
@@ -15,8 +15,11 @@ print("hello world 4")
 print("hello world 5")
 
 # Updates on mainbranch of GitHub
-print("hello world Updates on github)
+print("hello world Updates on github")
 
 
 # Updates in local master branch
 print("hello world Updates on local master branch")
+      
+# Updates on mainbranch of Github
+print("hello world Updates 2 on Github only")
